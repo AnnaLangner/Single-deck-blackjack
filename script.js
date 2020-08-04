04ddf259470b1db62b39fccf6ca951cd581612b1
Anna Langner <annalangner06@gmail.com>
2020-08-04 11:20:10 +0200

    Added the dealer functionality

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ function getShuffle(e) {
       const deck = JSON.parse(this.responseText);
       const deckId = deck.deck_id;  
 
-      getFirstTwoCard(deckId)
+      getPlayerFirstTwoCard(deckId)
+      getDealerFirstTwoCard(deckId)
     }
   }
 
@@ -23,7 +24,7 @@ function getShuffle(e) {
   e.preventDefault();
 }
 
-function getFirstTwoCard(deckId) {
+function getPlayerFirstTwoCard(deckId) {
   const urlFirstTwoCard = `${baseUrl}${deckId}/draw/?count=2`
 
   const xmlHttp = new XMLHttpRequest();
@@ -33,17 +34,21 @@ function getFirstTwoCard(deckId) {
   xmlHttp.onload = function() {
     if(this.status === 200){
       const deck = JSON.parse(this.responseText);
-      let sumValuesCards = 0;
+      let sumValuesPlayerCards = 0;
+
+      //Creating row table for cards
+      const rowPlayer = document.createElement('tr');
+      rowPlayer.className = 'table-body-content player'
+      document.getElementById('playerCards').appendChild(rowPlayer);
 
       for(let i = 0; i < deck.cards.length; i++) {
         const srcCard = deck.cards[i].image;
         const valueCard = deck.cards[i].value;
         let numValueCard;
 
-        const row = document.createElement('tr');
-        row.className = 'table-body-content'
-        row.innerHTML = `<td><img src="${srcCard + ' '}"></td>`;
-        document.getElementById('cardOutput').appendChild(row);
+        const cardItem = document.createElement('td')
+        cardItem.innerHTML = `<img src="${srcCard + ' '}">`;
+        rowPlayer.appendChild(cardItem)
 
         if(valueCard === 'JACK'){
           numValueCard = 2;
@@ -57,43 +62,100 @@ function getFirstTwoCard(deckId) {
           numValueCard = parseInt(valueCard)
         }
     
-        sumValuesCards += numValueCard;  
+        sumValuesPlayerCards += numValueCard;  
       }
 
-      const scoreCards = document.createElement('div');
-      scoreCards.className = 'card-body';
-      scoreCards.innerHTML = `
+      //Counting score
+      const scorePlayerCards = document.createElement('div');
+      scorePlayerCards.className = 'card-body';
+      scorePlayerCards.innerHTML = `
       <h5 class="card-title">Your score: </h5>
-      <p id="score">${sumValuesCards}</p>
+      <p id="scorePlayer">${sumValuesPlayerCards}</p>
       `
-      document.getElementById('scoreOutput').appendChild(scoreCards)
+      document.getElementById('playerScore').appendChild(scorePlayerCards)
 
-      const btnTakeCard = document.createElement('button');
-      btnTakeCard.onclick = function() {drawCard(deckId)};
-      btnTakeCard.innerHTML = 'Take the card';
-      btnTakeCard.className = 'btn btn-primary btn-lg';
-      btnTakeCard.setAttribute('type','button');
+      //Create buttons to continue the game and end the game.
+      const btnPlayerTakeCard = document.createElement('button');
+      btnPlayerTakeCard.onclick = function() {drawPlayerCard(deckId)};
+      btnPlayerTakeCard.innerHTML = 'Player take the card';
+      btnPlayerTakeCard.className = 'btn btn-primary btn-lg';
+      btnPlayerTakeCard.setAttribute('type','button');
 
-      const btnGameEnd = document.createElement('button');
-      btnGameEnd.onclick = function() {gameEnd(deckId)};
-      btnGameEnd.innerHTML = 'Quit the game';
-      btnGameEnd.className = 'btn btn-secondary btn-lg';
-      btnGameEnd.setAttribute('type','button');
+      const btnPlayerGameEnd = document.createElement('button');
+      btnPlayerGameEnd.onclick = function() {gameEnd(deckId)};
+      btnPlayerGameEnd.innerHTML = 'Player quit the game';
+      btnPlayerGameEnd.className = 'btn btn-secondary btn-lg';
+      btnPlayerGameEnd.setAttribute('type','button');
 
-      const continuePlaying = document.createElement('div');
-      continuePlaying.className = 'card-body';
-      continuePlaying.appendChild(btnTakeCard);
-      continuePlaying.appendChild(btnGameEnd);
+      const playerContinuePlaying = document.createElement('div');
+      playerContinuePlaying.className = 'card-body';
+      playerContinuePlaying.appendChild(btnPlayerTakeCard);
+      playerContinuePlaying.appendChild(btnPlayerGameEnd);
 
-      document.getElementById('btnOutput').appendChild(continuePlaying);
+      document.getElementById('btnOutput').appendChild(playerContinuePlaying);
     }
   }
 
   xmlHttp.send();
 }
 
+function getDealerFirstTwoCard(deckId) {
+  const urlFirstTwoCard = `${baseUrl}${deckId}/draw/?count=2`
 
-function drawCard(deckId) {
+  const xmlHttp = new XMLHttpRequest();
+
+  xmlHttp.open("GET", urlFirstTwoCard, true);
+
+  xmlHttp.onload = function() {
+    if(this.status === 200){
+      const deck = JSON.parse(this.responseText);
+      let sumValuesDealerCards = 0;
+
+      //Creating row table for cards
+
+      const rowDealer = document.createElement('tr');
+      rowDealer.className = 'table-body-content dealer'
+      document.getElementById('dealerCards').appendChild(rowDealer);
+
+      for(let i = 0; i < deck.cards.length; i++) {
+        const srcCard = deck.cards[i].image;
+        const valueCard = deck.cards[i].value;
+        let numValueCard;
+
+        const cardItem = document.createElement('td')
+        cardItem.innerHTML = `<img src="${srcCard + ' '}">`;
+        rowDealer.appendChild(cardItem)
+
+        if(valueCard === 'JACK'){
+          numValueCard = 2;
+        } else if (valueCard === 'QUEEN') {
+          numValueCard = 3;
+        } else if (valueCard === 'KING') {
+          numValueCard = 4;
+        } else if (valueCard === 'ACE') {
+          numValueCard = 11; 
+        } else {
+          numValueCard = parseInt(valueCard)
+        }
+    
+        sumValuesDealerCards += numValueCard;  
+      }
+
+      //Counting score
+      const scoreDealerCards = document.createElement('div');
+      scoreDealerCards.className = 'card-body';
+      scoreDealerCards.innerHTML = `
+      <h5 class="card-title">Dealer score: </h5>
+      <p id="scoreDealer">${sumValuesDealerCards}</p>
+      `
+      document.getElementById('dealerScore').appendChild(scoreDealerCards)      
+    }
+  }
+
+  xmlHttp.send();
+}
+
+function drawPlayerCard(deckId) {
   const urlTakenCard = `${baseUrl}${deckId}/draw/?count=1`;
 
   const xmlHttp = new XMLHttpRequest();
@@ -107,10 +169,9 @@ function drawCard(deckId) {
       const cardNewValue = deck.cards[0].value;
       let cardNewValueNum;
 
-      const cardNew = document.createElement('tr');
-      cardNew.className = 'table-body-content';
-      cardNew.innerHTML = `<td><img src="${cardNewSrc + ' '}"></td>`;
-      document.getElementById('cardOutput').appendChild(cardNew);
+      const cardNew = document.createElement('td');
+      cardNew.innerHTML = `<img src="${cardNewSrc + ' '}">`;
+      document.querySelector('.player').appendChild(cardNew);
 
       if(cardNewValue === 'JACK'){
         cardNewValueNum = 2;
@@ -124,9 +185,48 @@ function drawCard(deckId) {
         cardNewValueNum = parseInt(cardNewValue)
       }
   
-      let cardValueSum = document.getElementById('score').innerHTML;     
+      let cardValueSum = document.getElementById('scorePlayer').innerHTML;     
       let cardNewValueSum = parseInt(cardValueSum) + cardNewValueNum;  
-      document.getElementById('score').innerHTML = `${cardNewValueSum}`;
+      document.getElementById('scorePlayer').innerHTML = `${cardNewValueSum}`;
+    }
+  }
+
+  xmlHttp.send();
+}
+
+function drawDealerCard(deckId) {
+  const urlTakenCard = `${baseUrl}${deckId}/draw/?count=1`;
+
+  const xmlHttp = new XMLHttpRequest();
+
+  xmlHttp.open("GET", urlTakenCard, true);
+
+  xmlHttp.onload = function() {
+    if(this.status === 200) {
+      const deck = JSON.parse(this.responseText);
+      const cardNewSrc = deck.cards[0].image;
+      const cardNewValue = deck.cards[0].value;
+      let cardNewValueNum;
+
+      const cardNew = document.createElement('td');
+      cardNew.innerHTML = `<img src="${cardNewSrc + ' '}">`;
+      document.querySelector('.dealer').appendChild(cardNew);
+
+      if(cardNewValue === 'JACK'){
+        cardNewValueNum = 2;
+      } else if (cardNewValue === 'QUEEN') {
+        cardNewValueNum = 3;
+      } else if (cardNewValue === 'KING') {
+        cardNewValueNum = 4;
+      } else if (cardNewValue === 'ACE') {
+        cardNewValueNum = 11; 
+      } else {
+        cardNewValueNum = parseInt(cardNewValue)
+      }
+  
+      let cardValueSum = document.getElementById('scoreDealer').innerHTML;     
+      let cardNewValueSum = parseInt(cardValueSum) + cardNewValueNum;  
+      document.getElementById('scoreDealer').innerHTML = `${cardNewValueSum}`;
     }
   }
 
